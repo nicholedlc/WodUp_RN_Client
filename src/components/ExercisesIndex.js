fce7380566c23160e42a33eb5e81495bd322e959
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-05-04 13:03:58 -0700

    Create CardSection component

--- src/components/ExercisesIndex.js
+++ src/components/ExercisesIndex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
+import CardSection from './CardSection';
 
 export default class ExercisesIndex extends Component {
   constructor (props) {
@@ -16,6 +17,18 @@ export default class ExercisesIndex extends Component {
       .catch(console.info)
   }
 
+  renderExercises () {
+    this.state.exercises.map((e, i) => {
+      return (
+        <CardSection >
+          <Text key={`exercise ${i}`}>
+            {e.name}
+          </Text>
+        </CardSection>
+      );
+    })
+  }
+
   componentWillMount () {
     return this.getExercises();
   }
@@ -23,8 +36,14 @@ export default class ExercisesIndex extends Component {
   render () {
     return (
       <ScrollView>
-        {this.state.exercises.map(e => {
-          return <Text key={e.name}>{e.name}</Text>
+        {this.state.exercises.map((e, i) => {
+          return (
+            <CardSection key={`exercise ${i}`}>
+              <Text>
+                {e.name}
+              </Text>
+            </CardSection>
+          );
         })}
       </ScrollView>
     );
